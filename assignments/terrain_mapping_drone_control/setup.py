4d13943d0ab07be9e4613ee7cd08c29d77169406
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2025-02-03 10:57:44 -0700

    terrain mapping assignment, basic spiral trajectory position controller code

--- assignments/terrain_mapping_drone_control/setup.py
+++ assignments/terrain_mapping_drone_control/setup.py
@@ -12,19 +12,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='your.email@example.com',
-    description='A ROS2 package for controlling PX4 drone in spiral trajectory',
+    description='A ROS2 package for executing spiral trajectory with PX4',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'terrain_mapping_controller = terrain_mapping_drone_control.terrain_mapping_controller:main',
+            'spiral_trajectory = terrain_mapping_drone_control.spiral_trajectory:main',
         ],
     },
 ) 
